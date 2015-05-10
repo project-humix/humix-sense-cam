0ecf6ec4e0ede06cf1453b6182985c6c49a006bf
JEFFREY LIU <liuch@tw.ibm.com>
2015-05-10 23:50:06 +0800

    output captured image as base64 encoding

--- cam.js
+++ cam.js
@@ -1,20 +1,36 @@
+var nats = require('nats').connect();
+var log = require('logule').init(module, 'cam');
+
 var fs = require('fs');
 
-function readFile (){
 
-    fs.readFile('./test.jpg', function read(err, data) {
-        if (err) {
-            throw err;
-        }
 
-        var base64Image = new Buffer(data, 'binary').toString('base64');
+nats.subscribe('humix.sense.cam.command', function(msg){
 
-	var output_image = { 'image': base64Image};        
-        console.log(JSON.stringify(output_image));
-        
+    log.info('received cam command:'+msg);
+
+    var command = JSON.parse(msg);
+
+    if(command && command.action === 'takePic'){
+
+        // taking a picture..
+
+        fs.readFile('./images/user.jpg', function read(err, data) {
+
+            if (err) {
+                log.error("error reading cam image. abort.")
+                throw err;
+            }
+
+            var base64Image = new Buffer(data, 'binary').toString('base64');
+
+	        var output_image = { 'image': base64Image};        
+            nats.publish('humix.sense.cam.event', JSON.stringify(output_image));
     });
 
+        
+    }
     
-}
+});
 
-//readFile();
+
